b917ff391c2390922abd1baa988ff1275af137e8
Deshan Charuka <d.c.0729439631@gmail.com>
2021-01-19 14:50:25 +0530

    Initial setup

---
+++ src/main/java/lk/ijse/dep/orm/annotation/Column.java
@@ -0,0 +1,16 @@
+package lk.ijse.dep.orm.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author : Deshan Charuka <d.c.0729439631@gmail.com>
+ * @since : 2021-01-19
+ **/
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Column {
+    String value() default "";
+}
