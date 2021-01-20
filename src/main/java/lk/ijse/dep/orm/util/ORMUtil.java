66b17ce169120ac0ac10ffde77964da10f543fe9
Deshan Charuka <d.c.0729439631@gmail.com>
2021-01-20 08:38:37 +0530

    Finalize ORM

--- src/main/java/lk/ijse/dep/orm/util/ORMUtil.java
+++ src/main/java/lk/ijse/dep/orm/util/ORMUtil.java
@@ -7,6 +7,9 @@ import lk.ijse.dep.orm.annotation.Id;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 /**
@@ -19,9 +22,19 @@ public class ORMUtil {
         String password = dbProperties.getProperty("javax.persistence.jdbc.password");
         String url = dbProperties.getProperty("javax.persistence.jdbc.url");
         String driverClass = dbProperties.getProperty("javax.persistence.jdbc.driver_class");
+        Connection connection = null;
+        String sqlScript = "";
+
+        if (url.indexOf('?') == -1){
+            url += "?allowMultiQueries=true";
+        }else{
+            if (!url.contains("allowMultiQueries=true")){
+                url += "&allowMultiQueries=true";
+            }
+        }
 
         if (username == null | password == null | url == null | driverClass == null) {
-//            throw new RuntimeException("Unable to initialize ORM Framework without user credentials");
+            throw new RuntimeException("Unable to initialize ORM Framework without user credentials");
         }
 
         for (Class entity : entities
@@ -76,10 +89,19 @@ public class ORMUtil {
                     ddl += ",\n";
                 }
             }
-            ddl += ");";
-            System.out.println(ddl);
-        }
 
+            //To remove extra (, and new line)
+            ddl = ddl.substring(0, ddl.length() - 2) +  ");\n";
+            sqlScript += ddl;
+        }
+        try {
+            Statement stm = connection.createStatement();
+            System.out.println(sqlScript);
+            stm.execute(sqlScript);
+            connection.close();
+        } catch (SQLException throwables) {
+            throw new RuntimeException("Failed to create tables", throwables);
+        }
 
     }
 
